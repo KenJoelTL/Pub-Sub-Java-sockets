260c18b32e7b721d4a806cccf479a1d9fa2ce63a
Ken Joel L <joellutumba@hotmail.com>
2022-10-04 22:06:47 -0400

    implémentation des méthodes de publication

--- src/main/java/Classes/Publication.java
+++ src/main/java/Classes/Publication.java
@@ -7,8 +7,11 @@ import java.util.Map;
 import java.io.StringReader;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.json.JSONObject;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 /*
@@ -22,33 +25,73 @@ import org.xml.sax.InputSource;
  * @author AP57630
  */
 public class Publication implements IPublication{
-
+    // Le format Canonique est à changer pour qu'il prend en compte les types autres que STRING
     private Topic topic;
     private Map<String,String> content;
 
-    public Publication(Topic topic, Map<String,String> content) {
+    public Publication(Topic topic) {
         this.topic = topic;
-        this.content = content;
+    }
+    public Publication(Topic topic, Document xmlDocument) {
+        this.topic = topic;
+        this.fromXMLtoCanonical(xmlDocument);
+    }
+    public Publication(Topic topic, JSONObject json) {
+        this.topic = topic;
+        this.fromJSONtoCanonical(json);
     }
 
     @Override
     public void fromXMLtoCanonical(Document xmlDocument) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // var publicationNode = xmlDocument.getElementsByTagName("publication");
+        // var message = publicationNode.item(0);
+        var publication = xmlDocument.getFirstChild();
+        var message = publication.getFirstChild();
+        while(message != null) {
+            content.put(message.getNodeName(), message.getNodeValue());
+            message = message.getNextSibling();
+        }
     }
 
     @Override
     public Document fromCanonicaltoXML() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document xmlDoc = dBuilder.newDocument();
+
+            // Création de la balise racine
+            Element rootElement = xmlDoc.createElement("publication");
+            xmlDoc.appendChild(rootElement);
+
+            // Ajout du contenu de la publication dans des balises de 2e niveau
+            for (Map.Entry<String,String> entry : content.entrySet()) {
+                Element element = xmlDoc.createElement(entry.getKey());
+                element.appendChild(xmlDoc.createTextNode(entry.getValue()));
+                rootElement.appendChild(element);
+            }
+            return xmlDoc;
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public JSONObject fromCanonicaltoJSON() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        JSONObject jsonMessage = new JSONObject();
+        for (Map.Entry<String,String> entry : content.entrySet()) {
+            jsonMessage.put(entry.getKey(), entry.getValue());
+        }
+        return jsonMessage;
     }
 
     @Override
     public void fromJSONtoCanonical(JSONObject json) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.content.clear();
+        for (String key : json.keySet()) {
+            this.content.put(key, json.getString(key));
+        }
     }
 
 }
